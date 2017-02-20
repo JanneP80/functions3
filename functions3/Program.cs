9667b374d303a890d5a2c27e09edc1ff0204bb77
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-20 12:58:24 +0200

    change 1

--- functions3/Program.cs
+++ functions3/Program.cs
@@ -8,8 +8,34 @@ namespace functions3
 {
     class Program
     {
-        static void Main(string[] args)
+        static string numbercorrectness(int inputnumber)
         {
+            int checkit = inputnumber;
+            string checkmessage = string.Empty;
+            int inputnumber = int.TryParse(Console.ReadLine(), out inputnumber);
+            if (!inputnumber) // incorrect input
+            {
+                Console.Write("Input is NOT a number.");
+            }
+            else
+            if (inputnumber < 6 | inputnumber > 17)
+            {
+                checkmessage = ""
+            }
+            return checkmessage;
+
+        }
+
+            static void Main(string[] args)
+        {
+            int inputnumber = 0;
+            //string message = string.Empty;
+
+
+            Console.Write("Please give a number between 6 and 17: ");
+            Console.ReadLine(numbercorrectness(inputnumber)); // Calls and prints
+
+            Console.ReadKey();
         }
     }
 }
