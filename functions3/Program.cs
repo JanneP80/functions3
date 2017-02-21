fdf4c67640e513badbb2e26d41751c4061c075fb
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-21 09:03:43 +0200

    added refactored source

--- functions3/Program.cs
+++ functions3/Program.cs
@@ -8,34 +8,46 @@ namespace functions3
 {
     class Program
     {
-        static string numbercorrectness(int inputnumber)
+        static void Main(string[] args)
         {
-            int checkit = inputnumber;
-            string checkmessage = string.Empty;
-            int inputnumber = int.TryParse(Console.ReadLine(), out inputnumber);
-            if (!inputnumber) // incorrect input
-            {
-                Console.Write("Input is NOT a number.");
-            }
-            else
-            if (inputnumber < 6 | inputnumber > 17)
-            {
-                checkmessage = ""
-            }
-            return checkmessage;
-
-        }
-
-            static void Main(string[] args)
-        {
-            int inputnumber = 0;
-            //string message = string.Empty;
-
+            string checkersanswer;
 
             Console.Write("Please give a number between 6 and 17: ");
-            Console.ReadLine(numbercorrectness(inputnumber)); // Calls and prints
-
+            checkersanswer = numbercorrectness(6, 17);
+            Console.Write("Input {0} is valid number.", checkersanswer);
             Console.ReadKey();
+        }
+
+        static string numbercorrectness(int lowerlimit, int upperlimit)
+        {
+            int checkablenumber = 0;
+            string checkmessage = "0";
+            bool correctnumber = false;
+
+            do
+            {
+                string inputnumber = Console.ReadLine();               
+
+                if (Int32.TryParse(inputnumber, out checkablenumber))
+                {
+                    // checking number range here
+                    if (checkablenumber < lowerlimit | checkablenumber > upperlimit)
+                    {
+                        Console.Write("Input {0} is not a number between 6 and 17. Try again, please.", checkablenumber);                        
+                    }
+                    else
+                    {
+                        checkmessage = string.Format("{0}", checkablenumber); // correct input recieved -> allow continue
+                        correctnumber = true;
+                    }
+                }
+                else
+                {
+                    Console.Write("Input is not a valid number."); // not a number
+                }
+            } while (correctnumber == false);
+
+            return checkmessage; // number ?
         }
     }
 }
